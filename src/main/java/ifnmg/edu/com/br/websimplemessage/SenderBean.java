c2222822168cee6523cdf0a3c4c72afbd391fb6e
Lucas Flávio <lucasfgm00@gmail.com>
2023-06-23 15:06:23 -0300

    Seminario commit

---
+++ src/main/java/ifnmg/edu/com/br/websimplemessage/SenderBean.java
@@ -0,0 +1,80 @@
+package ifnmg.edu.com.br.websimplemessage;
+
+/**
+ *
+ * @author Lucas Flavio<lucasfgm at ifnmg.edu.br>
+ */
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+import jakarta.annotation.Resource;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.jms.*;
+
+@JMSDestinationDefinition(
+        name = "java:comp/jms/webappQueue",
+        interfaceName = "jakarta.jms.Queue",
+        destinationName = "PhysicalWebappQueue")
+@Named
+@RequestScoped
+public class SenderBean {
+    
+     static final Logger logger = Logger.getLogger("SenderBean");
+    @Inject
+    private JMSContext context;
+    @Resource(lookup = "java:comp/jms/webappQueue")
+    private Queue queue;
+    private String messageText;
+
+    /**
+     * Creates a new instance of SenderBean
+     */
+    public SenderBean() {
+    }
+
+    /**
+     * Get the value of messageText
+     *
+     * @return the value of messageText
+     */
+    public String getMessageText() {
+        return messageText;
+    }
+
+    /**
+     * Set the value of messageText
+     *
+     * @param messageText new value of messageText
+     */
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    /*
+     * Within a try-with-resources block, create context.
+     * Create message.
+     * Create producer and send message.
+     * Create FacesMessage to display on page.
+     */
+    public void sendMessage() {
+        try {
+            String text = "Message from producer: " + messageText;
+            context.createProducer().send(queue, text);
+
+            FacesMessage facesMessage =
+                    new FacesMessage("Sent message: " + text);
+            FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+        } catch (JMSRuntimeException t) {
+            logger.log(Level.SEVERE,
+                    "SenderBean.sendMessage: Exception: {0}",
+                    t.toString());
+        }
+    }
+    
+}
